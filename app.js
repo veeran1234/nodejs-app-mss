7f68a84fa441294ef3334d16eb57498a180fa062
root <root@ip-172-31-2-53.us-east-2.compute.internal>
2021-04-04 16:17:14 +0000

    changw html

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/mithuntechnologies', function(request, response) {
 app.get("/html", function(req,res){
     res.set("Content-Type","text/html");
     //res.contentType("html") ; 
-    res.write("<h2>Welcome</h2>");
+    res.write("<h2>Welsfsafasdfasdfasdgsdgcome</h2>");
     res.write("<h2>/html call</h2>");
     //must end 
     res.end();
